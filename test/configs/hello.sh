ba65d81589f0b3dd8889aa59a14fad5afd6377c2
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2020-09-13 10:24:11 -0400

    completes the hello demonstration.

--- test/configs/hello.sh
+++ test/configs/hello.sh
@@ -2,11 +2,11 @@ aws cloudformation create-stack \
     --stack-name Hello \
     --template-body file://${1} \
     --parameters \
-        ParameterKey=EnvironmentName,ParamaterValue=DevOpsProfessional,\
-        ParameterKey=ApplicationName,ParameterValue=Hello,\
-        ParameterKey=InputBucket,ParameterValue=jbennettconsulting,\
-        ParameterKey=InputKey,ParameterValue=codebuild.zip,\
-        ParameterKey=ArtifactBucket,ParameterValue=jbennettconsulting,\
-        ParameterKey=ArtifactPath,ParameterValue=artifacts,\
+        ParameterKey=EnvironmentName,ParameterValue=DevOpsProfessional \
+        ParameterKey=ApplicationName,ParameterValue=Hello \
+        ParameterKey=InputBucket,ParameterValue=jbennettconsulting \
+        ParameterKey=InputKey,ParameterValue=codebuild.zip \
+        ParameterKey=ArtifactBucket,ParameterValue=jbennettconsulting \
+        ParameterKey=ArtifactPath,ParameterValue=artifacts \
         ParameterKey=ArtifactKey,ParameterValue=hello.out \
-    --capabilities CAPABILITIES_NAMED_IAM
+    --capabilities CAPABILITY_NAMED_IAM
